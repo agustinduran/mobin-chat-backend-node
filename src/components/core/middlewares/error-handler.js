c36e1935ccf2084f6d0757f2128a4718b2171cc2
Agustín Durán <agustineduran@gmail.com>
2022-02-27 19:23:50 -0300

    Send error in json type

--- src/components/core/middlewares/error-handler.js
+++ src/components/core/middlewares/error-handler.js
@@ -8,7 +8,7 @@ const errorCatcher = (err, req, res, next) => {
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // Render the error message
-    res.status(err.status || 500).send('Servicio temporalmente no disponible');
+    res.status(err.status || 500).send({success: false, message: 'Servicio temporalmente no disponible'});
 };
 
 module.exports = { error404, errorCatcher };
