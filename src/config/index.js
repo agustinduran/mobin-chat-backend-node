014ecbf6cc58f6213aed5f11bc5b46561c4af5cc
Agustín Durán <agustineduran@gmail.com>
2022-02-27 19:24:01 -0300

    New packages

--- src/config/index.js
+++ src/config/index.js
@@ -2,11 +2,11 @@ require('dotenv').config();
 
 exports.config = {
     db: {
-        host: process.env.DB_HOST || 'localhost',
-        port: process.env.DB_PORT || '3306',
-        user: process.env.DB_USER || 'root',
+        host: process.env.DB_HOST         || 'localhost',
+        port: process.env.DB_PORT         || '3306',
+        user: process.env.DB_USER         || 'root',
         password: process.env.DB_PASSWORD || '',
-        database: process.env.DB_NAME || 'promiedos-fueguino'
+        database: process.env.DB_NAME     || 'promiedos-fueguino'
     },
     server: {
         port: process.env.SERVER_PORT || 3000,
