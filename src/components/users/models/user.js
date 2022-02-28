b96761a8f2b6379e237c091492cbecd03b37a1ef
Agustín Durán <agustineduran@gmail.com>
2022-02-28 03:09:32 -0300

    allowNull

--- src/components/users/models/user.js
+++ src/components/users/models/user.js
@@ -5,7 +5,7 @@ class User extends Model {}
 User.init({
     username: {type: DataTypes.STRING, allowNull: false},
     password: {type: DataTypes.STRING, allowNull: false},
-    email: {type: DataTypes.STRING, allowNull: false},
+    email: {type: DataTypes.STRING, allowNull: true},
     name: {type: DataTypes.STRING, allowNull: true},
     surname: {type: DataTypes.STRING, allowNull: true},
     phone: {type: DataTypes.STRING, allowNull: true},
