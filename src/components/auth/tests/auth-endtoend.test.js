c66b4eebcaed8c05f431f03ac24496b7dc09ee25
Agustín Durán <agustineduran@gmail.com>
2022-03-02 01:45:50 -0300

    Register tests

--- src/components/auth/tests/auth-endtoend.test.js
+++ src/components/auth/tests/auth-endtoend.test.js
@@ -1,12 +1,209 @@
 const request = require('supertest');
 const { app, server } = require('../../../index');
+const User = require('../../users/models/user');
 
 const api = request(app);
+
+beforeEach(async () => {
+    // FIXME: Warning with production database
+    await User.destroy({where: {}});
+    await User.create({
+        username: "agustin",
+        password: "$2a$10$k5C5nQxeDS3TSvD/Ya/rm.OWgdlaMTMeyc1So4JVUgLpQuiwvTObu", // 123456
+        email: "agustineduran@gmail.com",
+        name: "Agustín",
+        surname: "Durán",
+        phone: "2964547877"
+    });
+});
+
+describe('POST /auth/register', () => {
+    test('Register successful', async () => {
+        const body = {
+            "username": "vbuterin",
+            "password": "123456",
+            "password-confirmation": "123456",
+            "name": "Vitalik",
+            "surname": "Buterin",
+            "email" : "vitalik-buterin@gmail.com",
+            "phone" : "5349546546387"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(201);
+        expect(response.body.success).toBeTruthy();
+        expect(response.body.user).toBeDefined();
+    });
+
+    test('Register without body', async () => {
+        const body = {};
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register without username', async () => {
+        const body = {
+            "password": "123456",
+            "password-confirmation": "123456",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto@gmail.com",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register without password', async () => {
+        const body = {
+            "username": "snakamoto",
+            "password-confirmation": "123456",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto@gmail.com",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register without password-confirmation', async () => {
+        const body = {
+            "username": "snakamoto",
+            "password": "123456",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto@gmail.com",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register with differents password and password-confirmation', async () => {
+        const body = {
+            "username": "snakamoto",
+            "password": "123456",
+            "password-confirmation": "789123",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto@gmail.com",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register with passwords length less 6 characters', async () => {
+        const body = {
+            "username": "snakamoto",
+            "password": "12345",
+            "password-confirmation": "12345",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto@gmail.com",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register with invalid email', async () => {
+        const body = {
+            "username": "snakamoto",
+            "password": "123456",
+            "password-confirmation": "123456",
+            "name": "Satoshi",
+            "surname": "Nakamoto",
+            "email" : "satoshi-nakamoto",
+            "phone" : "5349219546548"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register with a username busy', async () => {
+        const body = {
+            "username": "agustin",
+            "password": "123456",
+            "password-confirmation": "123456",
+            "name": "Agustín",
+            "surname": "Durán",
+            "email" : "other-email@gmail.com",
+            "phone" : "2964547877"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+    test('Register with a email busy', async () => {
+        const body = {
+            "username": "other-username",
+            "password": "123456",
+            "password-confirmation": "123456",
+            "name": "Agustín",
+            "surname": "Durán",
+            "email" : "agustineduran@gmail.com",
+            "phone" : "2964547877"
+        };
+        const response = await 
+            api.post('/auth/register')
+               .send(body)
+               .set('Accept', 'application/json')
+               .expect('Content-Type', /json/);
+        expect(400);
+        expect(response.body.success).toBeFalsy();
+    });
+
+});
 
 describe('POST /auth/login', () => {
     test('Login by username successful', async () => {
         const body = {
-            username: "agustin2",
+            username: "agustin",
             password: "123456"
         };
         const response = await 
@@ -21,7 +218,7 @@ describe('POST /auth/login', () => {
 
     test('Login by email successful', async () => {
         const body = {
-            username: "agustinedura2n@gmail.com",
+            username: "agustineduran@gmail.com",
             password: "123456"
         };
         const response = await 
@@ -36,7 +233,7 @@ describe('POST /auth/login', () => {
 
     test('Login with password wrong using username', async () => {
         const body = {
-            username: "agustin2",
+            username: "agustin",
             password: "123456789"
         };
         await api
@@ -50,7 +247,7 @@ describe('POST /auth/login', () => {
 
     test('Login with password wrong using email', async () => {
         const body = {
-            username: "agustinedura2n@gmail.com",
+            username: "agustineduran@gmail.com",
             password: "123456789"
         };
         await api
@@ -163,8 +360,6 @@ describe('POST /auth/login', () => {
     // TODO: LOGIN WITH SQL INJECT
 
 });
-
-// TODO: REGISTER TEST
 
 afterAll(() => {
     server.close();
