0c5e59026d011dbfe584f1ae0a6ad41ffee65fe9
Agustín Durán <agustineduran@gmail.com>
2022-03-02 01:09:39 -0300

    Check isEmpty

--- src/components/auth/routes/auth-routes.js
+++ src/components/auth/routes/auth-routes.js
@@ -4,27 +4,31 @@ const router = express.Router();
 
 const authController = require('../controllers/auth-controller');
 
-const hasValidRequest                 = require('../../core/middlewares/error-request-handler');
-const { usernameExists, emailExists } = require('../../users/middlewares/email-username-exists');
+const hasValidRequest = require('../../core/middlewares/error-request-handler');
+const { usernameExists, emailExists, correctSamePasswords } = require('../../users/middlewares/email-username-exists');
 
 router.post('/login', [
-    check('username', 'Username es requerido').not().isEmpty(),
-    check('password', 'Contraseña es requerida').not().isEmpty(),
+    check('username', 'Usuario es requerido').not().isEmpty().trim().escape(),
+    check('password', 'Contraseña es requerida').not().isEmpty().trim().escape(),
     hasValidRequest
 ], authController.login);
 
 router.post('/register', [
-    check('username', 'Usuario es requerido').not().isEmpty(),
+    check('username', 'Usuario es requerido').not().isEmpty().trim().escape(),
     check('username').custom(usernameExists),
-    check('password', 'Contraseña es requerida').not().isEmpty(),
+    check('password', 'Contraseña es requerida').not().isEmpty().trim().escape(),
     check('password', 'Contraseña debe ser mayor a 6 caracteres').isLength({ min: 6 }),
     check('password', 'Contraseña debe ser menor a 20 caracteres').isLength({ max: 20 }),
-    check('email', 'Email es requerido').not().isEmpty(),
+    check('password-confirmation', 'Repetir contraseña es requerido').not().isEmpty().trim().escape(),
+    check('password-confirmation', 'Repetir contraseña debe ser mayor a 6 caracteres').isLength({ min: 6 }),
+    check('password-confirmation', 'Repetir contraseña debe ser menor a 20 caracteres').isLength({ max: 20 }),
+    check('password-confirmation').custom(correctSamePasswords),
+    check('email', 'Email es requerido').not().isEmpty().trim().escape(),
     check('email', 'Email ingresado inválido').isEmail(),
     check('email').custom(emailExists),
-    check('name', 'Nombre es requerido').not().isEmpty(),
-    check('surname', 'Apellido es requerido').not().isEmpty(),
-    check('phone', 'Teléfono es requerido').not().isEmpty(),
+    check('name', 'Nombre es requerido').not().isEmpty().trim().escape(),
+    check('surname', 'Apellido es requerido').not().isEmpty().trim().escape(),
+    check('phone', 'Teléfono es requerido').not().isEmpty().trim().escape(),
     hasValidRequest
 ], authController.register);
 
