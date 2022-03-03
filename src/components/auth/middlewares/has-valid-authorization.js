5c91c3993d5193e3293673a9d41594c1ef0c67a1
Agustín Durán <agustineduran@gmail.com>
2022-03-03 08:48:56 -0300

    Fix: var decode

--- src/components/auth/middlewares/has-valid-authorization.js
+++ src/components/auth/middlewares/has-valid-authorization.js
@@ -7,10 +7,11 @@ const hasValidAuthorization = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];
         const decode = jwt.verify(token, config.keys.jwt);
         // Save in the req for have a global state
-        req.userId = decoded.id;
+        req.userId = decode.id;
+        // TODO: add role, name
         return next();
     } catch (err) {
-        res.status(403).json({ success: false, message: 'No tiene permiso solicitado para acceder al recurso' });
+        res.status(403).json({ success: false, message: 'No tiene el permiso solicitado para acceder al recurso' });
     }
 };
 
