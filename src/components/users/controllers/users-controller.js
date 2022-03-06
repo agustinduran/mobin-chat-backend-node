56c5aa5cf619356c59d6f335e00f2e8464f88585
Agustín Durán <agustineduran@gmail.com>
2022-03-05 23:18:35 -0300

    set cache and todo: paginate

--- src/components/users/controllers/users-controller.js
+++ src/components/users/controllers/users-controller.js
@@ -1,22 +1,20 @@
+const cacheService    = require('../../core/services/cache-services');
 const usersService    = require('../services/users-service');
 const usersRepository = require('../repositories/users-mysql-repository');
 
 exports.getAll = async (req, res) => {
     const users = await usersService.getAll(usersRepository);
+    // TODO: PAGINATE
+    await cacheService.setCache(req.originalUrl, { success: true, users: users }, 60);
     res.status(200).json({ success: true, users: users });
 };
 
 exports.getById = async (req, res) => {
     const id = Number.parseInt(req.params.id);
-
-    // He dont want to see their resource?
-    if (id != req.userId /*TODO: && req.userRol == ADMIN */) {
-        return res.status(403).send({ success: false, message: 'No tiene el permiso solicitado para acceder al recurso' })
-    }
-
     const userFinded = await usersService.getUserById(usersRepository, id);
 
     if (userFinded) {
+        await cacheService.setCache(req.originalUrl, { success: true, user: userFinded }, 60);
         return res.status(200).json({ success: true, user: userFinded });
     } else {
         return res.status(404).send({ success: false, message: 'El recurso solicitado no existe o fue eliminado' })
