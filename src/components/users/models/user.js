90d5ea5202dbf4d93016400753a5224b0a0d26d1
Agustín Durán <agustineduran@gmail.com>
2022-03-05 23:16:52 -0300

    Change name role to roles

--- src/components/users/models/user.js
+++ src/components/users/models/user.js
@@ -3,16 +3,16 @@ const sequelize = require('../../../database/connection')
 
 class User extends Model {}
 User.init({
-    username : {type: DataTypes.STRING, allowNull: false},
-    password : {type: DataTypes.STRING, allowNull: false},
-    email    : {type: DataTypes.STRING, allowNull: true},
-    name     : {type: DataTypes.STRING, allowNull: true},
-    surname  : {type: DataTypes.STRING, allowNull: true},
-    phone    : {type: DataTypes.STRING, allowNull: true},
-    role     : {type: DataTypes.STRING, allowNull: true, default: '[]'},
-    active   : {type: DataTypes.TINYINT, allowNull: true, default: 1},
-    createdAt: {field: 'created_at', type: DataTypes.DATE, allowNull: true },
-    updatedAt: {field: 'updated_at', type: DataTypes.DATE, allowNull: true },
+    username : { type: DataTypes.STRING, allowNull: false },
+    password : { type: DataTypes.STRING, allowNull: false },
+    email    : { type: DataTypes.STRING, allowNull: true },
+    name     : { type: DataTypes.STRING, allowNull: true },
+    surname  : { type: DataTypes.STRING, allowNull: true },
+    phone    : { type: DataTypes.STRING, allowNull: true },
+    roles    : { type: DataTypes.STRING, allowNull: true, default: '["ROLE_USER"]' },
+    active   : { type: DataTypes.TINYINT, allowNull: true, default: 1 },
+    createdAt: { field: 'created_at', type: DataTypes.DATE, allowNull: true },
+    updatedAt: { field: 'updated_at', type: DataTypes.DATE, allowNull: true },
 }, {
     sequelize,
     modelName: 'users'
