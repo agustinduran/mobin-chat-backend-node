a95e7763914b6e5a5a84aab358181fed7942e275
Agustín Durán <agustineduran@gmail.com>
2022-03-05 23:18:09 -0300

    Verify permissions to user resource

---
+++ src/components/users/middlewares/verify-users-permissions.js
@@ -0,0 +1,23 @@
+const { ROLES } = require('../../core/enums/roles-enums');
+const rolesUtils = require('../../core/utils/roles-utils');
+
+const verifyPermissionsGetAllUsers = (req, res, next) => {
+    if (rolesUtils.hasRole(req.userRol, ROLES.ADMIN) == false) {
+        return res.status(403).send({ success: false, message: 'No tiene el permiso solicitado para acceder al recurso' })
+    } else {
+        next();
+    }
+};
+
+const verifyPermissionsGetOneUser = (req, res, next) => {
+    const id = Number.parseInt(req.params.id);
+
+    // He dont want to see their resource?
+    if (id != req.userId && rolesUtils.hasRole(req.userRol, ROLES.ADMIN) == false) {
+        return res.status(403).send({ success: false, message: 'No tiene el permiso solicitado para acceder al recurso' })
+    } else {
+        next();
+    }
+};
+
+module.exports = { verifyPermissionsGetAllUsers, verifyPermissionsGetOneUser };
