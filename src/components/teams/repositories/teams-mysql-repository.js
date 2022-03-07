3c14b0b8da47cfc5d4e83b00436c1c8a6085c2c9
Agustín Durán <agustineduran@gmail.com>
2022-03-07 14:55:54 -0300

    Team repository

--- src/components/teams/repositories/teams-mysql-repository.js
+++ src/components/teams/repositories/teams-mysql-repository.js
@@ -1,15 +1,41 @@
+const Team = require('../models/Team');
+
 exports.getAll = async () => {
-    return await [];
+    // TODO: Paginate
+    return await Team.findAll({ where: { active: 1 } });
 };
 
 exports.getById = async (id) => {
-    return await  [];
+    return await Team.findOne({ where: { id: id } });
 }
 
+// TODO: Should be more than one with same name
 exports.getByName = async (name) => {
-    return await [];
+    return await Team.findOne({ where: { nameShort: name } });
 }
 
 exports.save = async (team) => {
-    return await [];
+    return await Team.create({
+        nameShort: team.nameShort,
+        nameLong: team.nameLong,
+        fundationAt: team.fundationAt
+    });
+}
+
+exports.update = async (team) => {
+    // return await Team.create({
+    //     nameShort: team.nameShort,
+    //     nameLong: team.nameLong,
+    //     fundationAt: team.fundationAt
+    // });
+    return false;
+}
+
+exports.delete = async (team) => {
+    // return await Team.create({
+    //     nameShort: team.nameShort,
+    //     nameLong: team.nameLong,
+    //     fundationAt: team.fundationAt
+    // });
+    return false;
 }
